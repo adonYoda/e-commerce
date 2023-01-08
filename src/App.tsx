218e09071141344bfdc744d014d1b3b15f40222b
belmoz <sosnin.serg@gmail.com>
2023-01-08 18:21:37 +0200

    i libs

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,15 @@
 import React from 'react';
+import styled from 'styled-components';
 import './App.css';
 
+const Comp = styled.div`
+	width: 100%;
+	height: 100%;
+	background-color: red;
+`;
+
 function App() {
-	return <div className='App'></div>;
+	return <Comp className='App'></Comp>;
 }
 
 export default App;
