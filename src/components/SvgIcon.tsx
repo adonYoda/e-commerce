4861034d1ce44504c009d8d5652fe132cf513e69
adon yoda <vitredjer@gmail.com>
2023-01-12 16:33:26 +0200

    commit

---
+++ src/components/SvgIcon.tsx
@@ -0,0 +1,29 @@
+import { styled } from '@mui/material';
+import { FC, ReactNode } from 'react';
+import theme from 'src/utils/theme/theme';
+
+const SvgIconContainer = styled('span')<{ size?: number; color?: string }>`
+	& svg {
+		width: ${({ size }) => `${size}px` ?? `24px`};
+		height: ${({ size }) => `${size}px` ?? `24px`};
+		& path {
+			fill: ${({ theme, color }) => color ?? theme.palette.text.primary};
+		}
+	}
+`;
+
+interface Props {
+	size?: number;
+	color?: string;
+	icon: ReactNode;
+}
+
+const SvgIcon: FC<Props> = ({ size, color, icon }) => {
+	return (
+		<SvgIconContainer size={size} color={color}>
+			{icon}
+		</SvgIconContainer>
+	);
+};
+
+export default SvgIcon;
